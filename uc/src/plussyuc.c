c5819297015c7b7ac56861013d1dea292bce1890
Christian Carlowitz <cc@cmesh.de>
2015-09-04 23:41:02 +0200

    added (mostly) empty skeleton for uc code - lets an led blink

---
+++ uc/src/plussyuc.c
@@ -0,0 +1,39 @@
+/*
+	Plussy Display
+	Copyright (C) 2015  Christian Carlowitz
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+static void gpio_setup(void)
+{
+	rcc_periph_clock_enable(RCC_GPIOA);
+	gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO5);
+}
+
+int main(void)
+{
+	gpio_setup();
+
+	while (1)
+	{
+		gpio_toggle(GPIOA, GPIO5);
+		for (volatile int i = 0; i < 1000000; i++);
+	}
+
+	return 0;
+}
